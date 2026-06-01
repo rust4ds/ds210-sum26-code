05827527e3eb5cbfbc691140f6d35cef5ebf4742
valrevelin <valrev@bu.edu>
2026-06-01 19:41:34 -0400

    solution for v4

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -16,16 +16,27 @@ impl ChatbotV4 {
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
         let filename = &format!("{}.txt", username);
 
-        let mut chat_session: Chat<Llama> = self.model
-            .chat()
-            .with_system_prompt("The assistant will act like a pirate");
-
         // TODO: You have to implement the rest:
         // You need to load the chat session from the file using file_library::load_chat_session_from_file(...).
         // Think about what needs to happen if the function returns None vs Some(session).
         // Hint: look at https://docs.rs/kalosm/latest/kalosm/language/struct.Chat.html#method.with_session
 
-        return String::from("Hello, I am not a bot (yet)!");
+        let mut chat_session: Chat<Llama> = match file_library::load_chat_session_from_file(filename) {
+            None => {
+                self.model.chat().with_system_prompt("The assistant will act like a pirate")
+            }
+            Some(session) => {
+                let fresh_chat = self.model.chat().with_system_prompt("The assistant will act like a pirate");
+                fixed_load_session(fresh_chat, session)
+            }
+        };
+
+        let output = chat_session.add_message(message).await.unwrap();
+        let session = chat_session.session().unwrap();
+        let history = session.history();
+        println!("{:?}", history.len());
+        file_library::save_chat_session_to_file(filename, &session);
+        return output;
     }
 
     pub fn get_history(&self, username: String) -> Vec<String> {
@@ -37,7 +48,10 @@ impl ChatbotV4 {
             },
             Some(session) => {
                 // TODO: what should happen here?
-                return Vec::new();
+                return session.history().iter()
+                            .filter(|msg| msg.role() != MessageType::SystemPrompt)
+                            .map(|msg| msg.content().to_string())
+                            .collect();
             }
         }
     }
