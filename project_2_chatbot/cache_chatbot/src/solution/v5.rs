2b2c3bda9ba74e1a208d40c974545476e2193c5d
valrevelin <valrev@bu.edu>
2026-06-01 20:41:57 -0400

    solution for v5

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -25,13 +25,29 @@ impl ChatbotV5 {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
                 // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+                let mut chat_session = match file_library::load_chat_session_from_file(filename) {
+                    None => {
+                        self.model.chat().with_system_prompt("The assistant will act like a pirate")
+                    }
+                    Some(session) => {
+                    let fresh_chat = self.model.chat().with_system_prompt("The assistant will act like a pirate");
+                    fixed_load_session(fresh_chat, session)
+                    }
+                };
+                let output = chat_session.add_message(message).await.unwrap();
+                let chat_session_clone = chat_session.clone();
+                let session = chat_session_clone.session().unwrap();
+                file_library::save_chat_session_to_file(filename, &session);
+                self.cache.put(username.clone(), chat_session);
+                return output;
             }
             Some(chat_session) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
                 // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
-
+                let output = chat_session.add_message(message).await.unwrap();
+                let session = chat_session.session().unwrap();
+                file_library::save_chat_session_to_file(filename, &session);
+                return output;
             }
         }
     }
@@ -45,14 +61,28 @@ impl ChatbotV5 {
                 println!("get_history: {username} is not in the cache!");
                 // TODO: The cache does not have the chat. What should you do?
                 // Your code goes here.
-                return Vec::new();
+                match file_library::load_chat_session_from_file(filename) {
+                    None => {
+                        return Vec::new();
+                    }
+                    Some(session) => {
+                        let history = session.history().iter().skip(1).map(|msg| msg.content().to_string()).collect();
+                        let fresh_chat = self.model.chat().with_system_prompt("The assistant will act like a pirate");
+                        let chat_session = fixed_load_session(fresh_chat, session);
+                        self.cache.put(username.clone(), chat_session);
+                        return history;
+                    }
+                }
             }
             Some(chat_session) => {
                 println!("get_history: {username} is in the cache! Nice!");
                 // TODO: The cache has this chat. What should you do?
                 // Your code goes here.
-                return Vec::new();
-
+                let session = chat_session.session().unwrap();
+                return session.history().iter()
+                            .filter(|msg| msg.role() != MessageType::SystemPrompt)
+                            .map(|msg| msg.content().to_string())
+                            .collect();   
             }
         }
     }
